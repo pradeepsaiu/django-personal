3e831fe5b599b53712bf17a61f402b9fb1eb8467
pradeepsaiu <puppula1@binghamton.edu>
2017-03-05 20:34:29 -0500

    changes static file settings

--- cs480/cs480/settings.py
+++ cs480/cs480/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    BASE_DIR + '/www/static',
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
