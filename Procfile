628e5d0ca534237710e93f758c87e114920c70b9
pradeepsaiu <puppula1@binghamton.edu>
2017-03-05 19:38:46 -0500

    added in right directory

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh -c "cd cs480 && gunicorn cs480.wsgi"
--- cs480/www/views.py
+++ cs480/www/views.py
@@ -3,5 +3,4 @@ from .models import projects
 # Create your views here.
 def index(request):
 	proj_list = projects.objects.all()
-
-	return render(request,'index.html',{'proj_list':proj_list}  )
+	return render(request,'index.html',{'proj_list':proj_list})
